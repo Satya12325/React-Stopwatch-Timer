b835ffb94fff205a7e9359914be637763a4660d6
Satyabrata Das <satyabrata_pw1_135@masai.school>
2021-12-01 14:35:45 +0530

    Google timer and stop watch

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Stopwatch from './Components/Stopwatch'
+import Timer from './Components/Timer'
+function App() {
+  return (
+    <div className="App App-header">
+      <Timer/>
+      <Stopwatch/>
+     
+    </div>
+  );
+}
+
+export default App;
